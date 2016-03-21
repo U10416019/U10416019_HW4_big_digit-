9be78fdf47359b3b31e0d1a5c11b374cf1a7c4bf
WenChi <u10416019@go.utaipei.edu.tw>
2016-03-21 22:40:50 +0800

    Create a scanner
    This file is create a scanner and print some sentences to let the user enter two numbers.

---
+++ TestBigNumber_Plus.java
@@ -0,0 +1,18 @@
+//U10416019鄭文琪
+
+import java.util.Scanner;
+
+public class TestBigNumber_Plus{
+	public static void main(String[] args){
+		//create a scanner
+		Scanner input = new Scanner(System.in);
+		
+		//let the user enter two numbers
+		System.out.println("Please enter two integer numbers to plus.");
+		System.out.println("First number : ");
+		String firstNumber = input.next();
+		System.out.println("Second number : ");
+		String secondNumber = input.next();
+
+	}
+}
