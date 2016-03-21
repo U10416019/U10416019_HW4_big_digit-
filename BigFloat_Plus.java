a584b9922e95aa3192df09505cd5b03f55345dab
WenChi <u10416019@go.utaipei.edu.tw>
2016-03-21 23:23:33 +0800

    Determine which arraylist is longer and plus them
    These two methods are determine which arraylist is longer, and judge for two parts, one is before the dot, and another is after the dot.  If this arraylist is smaller than another one, then add zero util its length is the same with another arraylist.  After that, plus these arraylists numbers in two different arraylists, one is before the dot, and another is after the dot.

--- BigFloat_Plus.java
+++ BigFloat_Plus.java
@@ -40,7 +40,29 @@ public class BigFloat_Plus{
 			secondInt.add(Integer.valueOf(String.valueOf(secondNumber.charAt(i))));
 		}
 	}
-
+	
+	//create a mutator to add the numbers before dot
+	public void setTotalInt(){
+		//determine which arraylist is longer
+		//if the second arraylist is smaller then add 0 util its length is the same with the first arraylist
+		if(firstInt.size() > secondInt.size()){
+			for(int i = secondInt.size(); i < firstInt.size(); i++){
+				secondInt.add(0);
+			}
+		}
+		//if the first arraylist is smaller then add 0 util its length is the same with the second arraylist
+		else if(secondInt.size() > firstInt.size()){
+			for(int i = firstInt.size(); i < secondInt.size(); i++){
+				firstInt.add(0);
+			}
+		}
+		
+		//add the numbers of two arraylist
+		for(int i = 0; i < firstInt.size(); i++){
+			totalInt.add(firstInt.get(i) + secondInt.get(i));
+		}
+	}
+	
 	//create a mutator to let the first string become integer and add them in arraylist after the dot
 	void setFirstFloat(){
 		for(int i = firstInt.size() + 1; i < firstNumber.length(); i++){
@@ -52,6 +74,28 @@ public class BigFloat_Plus{
 	void setSecondFloat(){
 		for(int i = secondInt.size()+1; i < secondNumber.length(); i++){
 			secondFloat.add(Integer.valueOf(String.valueOf(secondNumber.charAt(i))));
+		}
+	}
+	
+	//create a mutator to add the numbers after dot
+	public void setTotalFloat(){
+		//determine which arraylist is longer
+		//if the second arraylist is smaller then add 0 util its length is the same with the first arraylist
+		if(firstFloat.size() > secondFloat.size()){
+			for(int i = secondFloat.size(); i < firstFloat.size(); i++){
+				secondFloat.add(0);
+			}
+		}
+		//if the first arraylist is smaller then add 0 util its length is the same with the second arraylist
+		else if(secondFloat.size() > firstFloat.size()){
+			for(int i = firstFloat.size(); i < secondFloat.size(); i++){
+				firstFloat.add(0);
+			}
+		}
+		
+		//add the numbers of two arraylist
+		for(int i = 0; i < firstFloat.size(); i++){
+			totalFloat.add(firstFloat.get(i) + secondFloat.get(i));
 		}
 	}
 
